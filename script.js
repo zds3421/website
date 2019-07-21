c1f98dbc89a22a3aa9fda1164f5210afe7da9b27
Zander Swanepoel <52563131+ZanderDS@users.noreply.github.com>
2019-07-21 10:28:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,17 +5,20 @@
 
 function closeNav() {
 	document.getElementById('sidenav').style.width = '0';
-	document.getElementById('content').style.marginLeft = '0';
+	document.getElementById('hamburger').style.marginLeft = '0';
+	// document.getElementById('content').style.marginLeft = '0';
 }
 var isOut = false;
 function myFunction() {
 	var x = document.getElementById('sidenav');
 	if (isOut === true) {
 		document.getElementById('sidenav').style.width = '0';
-		document.getElementById('content').style.marginLeft = '0';
+		document.getElementById('hamburger').style.marginLeft = '0';
+		// document.getElementById('content').style.marginLeft = '0';
 	} else {
 		document.getElementById('sidenav').style.width = '160px';
-		document.getElementById('content').style.marginLeft = '160px';
+		document.getElementById('hamburger').style.marginLeft = '160px';
+		// document.getElementById('content').style.marginLeft = '160px';
 	}
 	isOut = !isOut;
 }
