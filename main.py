e8fa2f92e344d977ad27dc127103641bdf25c0bd
Zander Swanepoel <zander.swanepoel@shoprite.co.za>
2022-09-01 22:47:31 +0200

    changed base.html to index.html

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/ruan')
-def home():
-    return '<h1>bitch</h1>'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
